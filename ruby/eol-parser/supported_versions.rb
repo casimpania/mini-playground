fbdb1ffec5bb7080904df5ae682fa03d28248b59
Ismael Casimpan Jr <ismael.angelo@casimpan.com>
2022-02-24 21:54:16 +0800

    ruby code can now get directly from the net

--- ruby/eol-parser/supported_versions.rb
+++ ruby/eol-parser/supported_versions.rb
@@ -1,17 +1,48 @@
 #!/usr/bin/env ruby
 
-#sw_name=ARGV[0]  ## Read cli parameter input
+### Made mainly with the help of the following:
+###  * https://jhawthorn.github.io/curl-to-ruby/
+###  * https://gist.github.com/timsavery/1657351
+
+sw_name=ARGV[0]  ## Read cli parameter input
 
 ## include common config
+##
+## TODO:
+##   * Use these config
+##   * use same algorithm as the php version
 require_relative './conf/common.conf.rb'
 
-require 'json'
-#json_file="https://endoflife.date/api/#{sw_name}.json"
-json_file="./python.json"
+require "json"
+require "net/http"
+require "uri"
+require "date"
+json_url="https://endoflife.date/api/#{sw_name}.json"
 
-raw_data=File.read(json_file)
-parsed_data = JSON.parse(raw_data)
+uri = URI.parse(json_url)
+response = Net::HTTP.get_response(uri)
 
-puts parsed_data['eol']
-#puts parsed_data.keys
+year=DateTime.now.year.to_s
+month=DateTime.now.month.to_s
+day=DateTime.now.day.to_s
+date_now="#{year}-#{month}-#{day}"
 
+if response.code == "200"
+  result = JSON.parse(response.body)
+
+  result.each do |each_entry|
+    if each_entry.fetch("eol") > date_now
+        each_entry.each {
+            |k,v|
+            puts "#{k} = #{v}"
+        }
+    else
+        next ## skip those without "eol"
+    end
+    ## Put separator for next group...
+    10.times { print '-' }
+    print "\n"
+  end
+else
+  puts "ERROR!!!"
+end
